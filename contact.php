3323268363556c323303b4f8756e3cb0a3377b72
Praisy <109698690+praisechukwuebuka@users.noreply.github.com>
2022-09-11 20:12:12 +0100

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,23 @@
+<?Php 
+        //creating the variables for the contact
+if (isset($_POST['submit'])){
+    $name = trim($_POST['name']);
+    $email = trim($_POST['email']);
+    $subject = trim($_POST['subject']);
+    $message = trim($_POST['message']);
+
+    //needed details (For the location of my prey's details), also making it more readable for me
+    $myMail = "Praisebuka@poton.me";
+    $header = "From: " . $email;
+    $message2 = "Here is a new message from " . $name . " with the email of ". $email . ".\n\n" . $message;
+
+    
+    //Writing the functions for where the above details should go
+    //such as.....The email am sending to
+    //............The subject
+    //............The message
+    mail($myMail, $subject, $message2);
+    header("Location: index.html?Successful");
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,23 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Them fams</title>
+     <link rel="stylesheet" type="text/css" href="contact.css">
+</head>
+<body>
+     
+    <div>
+        <h1 class="topic">Get In Touch With Praise</h1>
+        <p class="words">Please fill in these fields correctly</p>
+        <form action="contact.php" method="post">
+            <input type="text" name="name" placeholder="Fullname">
+            <input type="text" name="email" placeholder="E-mail">
+            <input type="text" name="subject" placeholder="Subject">
+            <textarea name="message" placeholder="Enter Message"></textarea>
+            <button type="submit" name="submit">Send Details</button>
+        </form>
+    </div>
+</body>
+</html>
