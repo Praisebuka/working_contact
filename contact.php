d2347344e777412e8b5bac7388aba5ccc53fe4f5
Praise Ebuka <109698690+Praisebuka@users.noreply.github.com>
2022-11-06 20:28:01 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,12 +1,4 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Praisebuka</title>
-</head>
 
-<body>
         <?Php 
         //creating the variables for the contact
 if (isset($_POST['submit'])){
@@ -28,5 +20,4 @@ if (isset($_POST['submit'])){
     mail($myMail, $subject, $message2);
     header("Location: index.html?Successful");
 }
-        </body>
-</html>
+     
