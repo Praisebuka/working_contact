1c1771a15b0acae081cbc68d9f43c279fbfd4dfc
Praise Ebuka <109698690+Praisebuka@users.noreply.github.com>
2022-11-06 20:08:24 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,15 +3,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>Them fams</title>
+     <title>Them my-guys</title>
      <link rel="stylesheet" type="text/css" href="contact.css">
 </head>
 <body>
      
     <div>
-        <h1 class="topic">Get In Touch With Praise</h1>
+        <h1 class="topic">Get In Touch With Praisebuka</h1>
         <p class="words">Please fill in these fields correctly</p>
-        <form action="contact.php" method="post">
+        <form action="contact.html" method="post">
             <input type="text" name="name" placeholder="Fullname">
             <input type="text" name="email" placeholder="E-mail">
             <input type="text" name="subject" placeholder="Subject">
